71f74429f97e77f0d244f07ddb255a2696d841e0
Andrew Howran <andrewhowrandev17@gmail.com>
2022-08-15 13:10:57 -0400

    rehaul of hero image

--- client/src/components/Sections/Hero.jsx
+++ client/src/components/Sections/Hero.jsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <div className="landing_background flex justify-center items-center min-h-screen">
-      <div className="flex flex-col space-y-1 mb-56 pl-96 ml-36">
-        <p className="text-6xl font-extrabold ">Stunning Products</p>
-        <p className="text-4xl font-extralight tracking-wider">
-          Made The Right Way
-        </p>
+    <div className="p-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2  sm:space-x-7">
+        <div className="justify-self-center order-2 sm:order-1">
+          <img
+            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            alt=""
+            className="hero_height"
+          />
+        </div>
+        <div className=" flex flex-col space-y-2 justify-center mb-10 sm:mb-20 text-center sm:text-start order-1 sm:order-2">
+          <h5 className="font-extrabold text-6xl sm:text-4xl md:text-5xl lg:text-7xl">
+            Your One{" "}
+            <span className="text-main_teal sm:whitespace-nowrap">
+              Stop Shop
+            </span>
+          </h5>
+          <p className="tracking-wider font-medium">
+            Explore Everything From Clothing, Jewelery And Technology
+          </p>
+
+          <Link to="/products">
+            <button
+              className={
+                " bg-transparent hover:bg-main_teal text-main_teal font-semibold hover:text-main_white py-2 px-12 border border-main_teal hover:border-transparent duration-200 "
+              }
+            >
+              View Products
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -12,9 +12,9 @@ export default function Home() {
       <NavContainer />
       <Hero />
       <SectionTwo />
-      <About />
+      {/* <About />
       <SectionOne />
-      <SectionThree />
+      <SectionThree /> */}
     </>
   );
 }
