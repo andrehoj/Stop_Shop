fc9392cd49614612820e8fe3090204d9278c7626
Andrew Howran <andrewhowrandev17@gmail.com>
2022-08-18 10:20:09 -0400

    removed stripe

--- server/server.js
+++ server/server.js
@@ -37,28 +37,3 @@ async function startApolloServer(typeDefs, resolvers) {
 
 startApolloServer(typeDefs, resolvers);
 
-
-// // Don’t submit any personally identifiable information in requests made with this key.
-// // Sign in to see your own test API key embedded in code samples.
-// const stripe = require('stripe')('sk_test_Hrs6SAopgFPF0bZXSN3f6ELN');
-
-// const YOUR_DOMAIN = '';
-
-// app.post('/create-checkout-session', async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//         price: '{{PRICE_ID}}',
-//         quantity: 1,
-//       },
-//     ],
-//     mode: 'payment',
-//     success_url: `${YOUR_DOMAIN}?success=true`,
-//     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-//   });
-
-//   res.redirect(303, session.url);
-// });
-
-// app.listen(4242, () => console.log('Running on port 4242'));
