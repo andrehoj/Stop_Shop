156660878f4d430f1459504fa497d3ac29269d3d
Andrew Howran <andrewhowrandev17@gmail.com>
2022-08-18 21:22:15 -0400

    seed updated

--- server/utils/seeds/seeds.js
+++ server/utils/seeds/seeds.js
@@ -1,57 +1,62 @@
 const mongoose = require("mongoose");
+const db = require("../../config/connection");
 const { Product, User } = require("../../models/index");
 const fetch = require("node-fetch");
 
-fetch("https://fakestoreapi.com/products?limit=50")
-  .then((res) => {
-    return res.json();
-  })
-  .then((products) => {
-    mongoose
-      .connect("mongodb://127.0.0.1:27017/MERN_Products", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log("MONGODB seeding process started!");
-        seedDB().then(() => {
-          mongoose.connection.close();
+db.once("open", () => {
+  fetch("https://fakestoreapi.com/products?limit=50")
+    .then((res) => {
+      return res.json();
+    })
+    .then((products) => {
+      mongoose
+        .connect(
+          process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/MERN_Products",
+          {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }
+        )
+        .then(() => {
+          console.log("MONGODB seeding process started!");
+          seedDB().then(() => {
+            mongoose.connection.close();
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
 
-    async function seedDB() {
-      await Product.deleteMany();
-      await User.deleteMany();
+      async function seedDB() {
+        await Product.deleteMany();
+        await User.deleteMany();
 
-      await Product.insertMany(products);
-      await User.insertMany([
-        {
-          firstName: "Andrew",
-          lastName: "Howran",
-          email: "ahole@gmail.com",
-          password: "mya",
-        },
+        await Product.insertMany(products);
+        await User.insertMany([
+          {
+            firstName: "Andrew",
+            lastName: "Howran",
+            email: "andrew@gmail.com",
+            password: "mya",
+          },
 
-        {
-          firstName: "Brianna",
-          lastName: "Howran",
-          email: "briannabanana@gmail.com",
-          password: "ginger",
-        },
+          {
+            firstName: "Brianna",
+            lastName: "Howran",
+            email: "briannabanana@gmail.com",
+            password: "ginger",
+          },
 
-        {
-          firstName: "John",
-          lastName: "Doe",
-          email: "johndoe@gmail.com",
-          password: "password",
-        },
-      ]);
-    }
-  });
-
+          {
+            firstName: "John",
+            lastName: "Doe",
+            email: "johndoe@gmail.com",
+            password: "password",
+          },
+        ]);
+      }
+    });
+});
 // })
 // .catch(function handleError(error) {
 //   console.log("Error" + error);
