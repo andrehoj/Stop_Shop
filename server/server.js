21f6c5ae384e571da97f75eaa9d27a837f410cee
Andrew Howran <andrewhowrandev17@gmail.com>
2023-01-26 16:44:57 -0500

    added cache to apollo server constructor

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMidWare,
+  cache: "bounded",
 });
 
 const app = express();
@@ -21,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
