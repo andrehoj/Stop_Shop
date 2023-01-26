9757583a6a6e91ddced51b0d4461f0cfff0c8906
Andrew Howran <andrewhowrandev17@gmail.com>
2023-01-26 16:04:35 -0500

    change build folder

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 async function startApolloServer(typeDefs, resolvers) {
   await server.start();
